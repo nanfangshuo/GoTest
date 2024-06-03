6e8fb8ebb4087f8d46eb4992a11c7ce210ed9466
nanfangshuo <1648311881@qq.com>
2024-06-03 20:01:43 +0800

    结束送风请求

---
+++ WindRequest/StopWind.go
@@ -0,0 +1,26 @@
+package WindRequest
+
+import (
+	"GoTest/HttpRequest"
+	"fmt"
+)
+
+//停止送风
+
+func StopWind() error {
+	var requestBody map[string]interface{}
+	var response map[string]interface{}
+	err, responseStatus := HttpRequest.SendPostRequestWithToken("/room/blowing/stop", requestBody, &response)
+	if err != nil {
+		fmt.Println("停止送风请求发送错误：", err)
+		return err
+	}
+	if responseStatus == 200 {
+		fmt.Println("发送停止送风请求成功")
+		//TODO：停止送风的操作
+		return nil
+	} else {
+		fmt.Println("停止送风请求失败：", response["message"])
+	}
+	return nil
+}

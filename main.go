ce5e17ba12ae6cddee9c0c38bc887ddfe910b84a
nanfangshuo <1648311881@qq.com>
2024-06-11 16:21:02 +0800

    update

--- main.go
+++ main.go
@@ -17,51 +17,8 @@ func main() {
 
 	// 开启一个线程，每4/RefreshSpeed秒向主控机汇报从控机的状态，每秒检查温度是否需要发起请求
 	quit := make(chan struct{})
-	go func() {
-		ticker1 := time.NewTicker(3 * time.Second / time.Duration(RefreshSpeed))
-		ticker2 := time.NewTicker(1 * time.Second)
-		defer ticker1.Stop()
-		defer ticker2.Stop()
-		for {
-			select {
-			case <-ticker1.C: //每6/RefreshSpeed秒一次,汇报从控机温度并获取从控机费用、主控机模式、刷新速率
-				// 调用ReportStatus函数
-				err, workStatus, refreshSpeed := Room.ReportStatus(room.WorkStatus, room.Temperature)
-				if err != nil {
-					// 处理错误
-					fmt.Println("ReportStatus error:", err)
-				} else {
-					//若工作模式改变
-					if workStatus != room.WorkStatus {
-						Room.StopWind()
-						room.WorkStatus = workStatus
-						if room.WorkStatus == "Cool" {
-							room.TargetTemperature = 22
-						} else {
-							room.TargetTemperature = 28
-						}
-					}
-					//若刷新速率改变
-					if refreshSpeed != RefreshSpeed {
-						RefreshSpeed = refreshSpeed
-						ticker1.Stop()
-						ticker1 = time.NewTicker(3 * time.Second / time.Duration(RefreshSpeed))
-					}
-				}
-			case <-ticker2.C: //每秒一次，检查温度是否需要发起请求，闲时以每秒0.2度的速度回归20
-				Room.CheckTemperature(room)
-				if room.Temperature > 20.05 {
-					room.Temperature -= 0.05
-				} else if room.Temperature < 19.95 {
-					room.Temperature += 0.05
-				} else {
-					room.Temperature = 20
-				}
-			case <-quit:
-				return
-			}
-		}
-	}()
+	go reportStatusPeriodically(room, quit)
+	go checkTemperaturePeriodically(room, quit)
 	for {
 		fmt.Println("输入0注销")
 		fmt.Println("输入1查看房间当前状态")
@@ -118,4 +75,55 @@ end: //退出循环的标记
 		fmt.Println("Logout success")
 	}
 	close(quit) // 注销后关闭quit通道，使得goroutine停止运
+}
+
+func reportStatusPeriodically(room *Room, quit chan struct{}) {
+	ticker1 := time.NewTicker(3 * time.Second / time.Duration(RefreshSpeed))
+	defer ticker1.Stop()
+	for {
+		select {
+		case <-ticker1.C:
+			err, workStatus, refreshSpeed := Room.ReportStatus(room.WorkStatus, room.Temperature)
+			if err != nil {
+				fmt.Println("ReportStatus error:", err)
+			} else {
+				if workStatus != room.WorkStatus {
+					Room.StopWind()
+					room.WorkStatus = workStatus
+					if room.WorkStatus == "Cool" {
+						room.TargetTemperature = 22
+					} else {
+						room.TargetTemperature = 28
+					}
+				}
+				if refreshSpeed != RefreshSpeed {
+					RefreshSpeed = refreshSpeed
+					ticker1.Stop()
+					ticker1 = time.NewTicker(3 * time.Second / time.Duration(RefreshSpeed))
+				}
+			}
+		case <-quit:
+			return
+		}
+	}
+}
+
+func checkTemperaturePeriodically(room *Room, quit chan struct{}) {
+	ticker2 := time.NewTicker(1 * time.Second)
+	defer ticker2.Stop()
+	for {
+		select {
+		case <-ticker2.C:
+			Room.CheckTemperature(room)
+			if room.Temperature > 20.05 {
+				room.Temperature -= 0.05
+			} else if room.Temperature < 19.95 {
+				room.Temperature += 0.05
+			} else {
+				room.Temperature = 20
+			}
+		case <-quit:
+			return
+		}
+	}
 }
